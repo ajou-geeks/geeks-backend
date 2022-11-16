d80a8f92a88ce9539e83a7ecde21ed44175e5202
ajy9844 <ajy09844@gmail.com>
2022-11-17 01:06:42 +0900

    feat: create project

---
+++ src/main/java/com/geeks/geeksbackend/GeeksBackendApplication.java
@@ -0,0 +1,13 @@
+package com.geeks.geeksbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeeksBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeeksBackendApplication.class, args);
+	}
+
+}
